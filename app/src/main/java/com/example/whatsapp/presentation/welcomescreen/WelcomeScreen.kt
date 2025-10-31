871b8ea4e4529dd2f908bb5b398142a9a8ba225f
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-10-31 17:52:08 +0500

    initial commit

--- app/src/main/java/com/example/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
@@ -1,5 +1,112 @@
 package com.example.whatsapp.presentation.welcomescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.whatsapp.R
+import androidx.compose.ui.text.font.FontWeight
 
 
 
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun WelcomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.whatsapp_sticker),
+                contentDescription = null,
+                modifier = Modifier.size(300.dp)
+            )
+
+            Text(
+                text = "Welcome To WhatsApp",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(height = 20.dp))
+
+            Row {
+                Text(
+                    text = "Read our",
+                    color = Color.Gray
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "Privacy policy",
+                    color = colorResource(id = R.color.light_green)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "Tap 'Agree and Continue' to",
+                    color = Color.Gray
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+            }
+
+            Row {
+
+                Text(
+                    text = "accept the",
+                    color = Color.Gray
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "Term and Condition",
+                    color = colorResource(id = R.color.light_green)
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(height = 20.dp))
+
+            Button(
+                onClick = { /* TODO */ },
+                modifier = Modifier
+                    .width(260.dp)
+                    .height(43.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.dark_green)
+                )
+            )
+            {
+                Text(text = "Agree and Continue", color = Color.White)
+            }
+
+
+        }
+    }
+}
