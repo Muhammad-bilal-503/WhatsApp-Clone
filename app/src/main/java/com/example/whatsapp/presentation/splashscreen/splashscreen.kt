c2261a9b227a49c366f8f84d4410d0840372552e
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-10-31 09:03:20 +0500

    initail Commit

---
+++ app/src/main/java/com/example/whatsapp/presentation/splashscreen/splashscreen.kt
@@ -0,0 +1,76 @@
+package com.example.whatsapp.presentation.splashscreen
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.whatsapp.R
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.colorResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+
+
+
+
+
+@Composable
+fun SplashScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.whatsapp_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .size(80.dp)
+                .align(Alignment.Center) // 👈 centers the image
+        )
+
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "From",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.meta),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = colorResource(id = R.color.light_green) // ✅ Correct
+                )
+                Spacer(modifier = Modifier.width(5.dp)) // 👈 adds space between icon and text
+                Text(
+                    text = "Meta",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+
+                )
+            }
+
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
