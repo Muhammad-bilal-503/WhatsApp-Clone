f0e9d1fbc913223d2bd1cfe8ab3bcfafaefa0def
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-11-18 19:48:39 +0500

    initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
     id ("com.google.dagger.hilt.android")
     id ("kotlin-kapt")
+    alias(libs.plugins.google.gms.google.services)
 
 }
 
@@ -56,6 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.firebase.database)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
