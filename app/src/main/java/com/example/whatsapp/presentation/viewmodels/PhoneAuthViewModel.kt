6077c046e5d3f203367a23b51ad6326ef29cc9b7
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-11-27 19:44:17 +0500

    initial Commit

--- app/src/main/java/com/example/whatsapp/presentation/viewmodels/PhoneAuthViewModel.kt
+++ app/src/main/java/com/example/whatsapp/presentation/viewmodels/PhoneAuthViewModel.kt
@@ -1,5 +1,25 @@
 package com.example.whatsapp.presentation.viewmodels
 
-class PhoneAuthViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@HiltViewModel
+class PhoneAuthViewModel @Inject constructor() : ViewModel() {
+
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Ideal)
+    val authState = _authState
+
+
 
 }
+sealed class AuthState{
+    object Ideal : AuthState()
+    object Loading : AuthState()
+    data class CodeSent(val varificationId : String) : AuthState()
+    data class Success(val user :phoneAuthUser) : AuthState()
+    data class Error(val message : String) : AuthState()
+}
+
+
