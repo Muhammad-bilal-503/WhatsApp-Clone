4c02dc15bbbbffc3b9d958cf8f8d008898f7f237
Muhammad-bilal-503 <mughalbillal0012345@gmail.com>
2025-12-03 18:05:17 +0500

    initial Commit

--- app/src/main/java/com/example/whatsapp/presentation/profile/UserProfileSetScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/profile/UserProfileSetScreen.kt
@@ -1,6 +1,8 @@
 package com.example.whatsapp.presentation.profile
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.whatsapp.presentation.viewmodels.PhoneAuthViewModel
@@ -9,5 +11,9 @@ import com.example.whatsapp.presentation.viewmodels.PhoneAuthViewModel
 @Preview(showBackground = true, showSystemUi = true)
 fun userProfileSetScreen(phoneAuthViewModel: PhoneAuthViewModel, navHostController: NavHostController){
 
+
+    var name by remember { mutableStateOf("") }
+
+    var status by remember { mutableStateOf("") }
 
 }
